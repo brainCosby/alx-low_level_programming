9e256e5e8dd37c271644d90613b2afaaf40b2f2d
brainCosby <braincosby777@gmail.com>
2023-07-17 16:47:57 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - The main function is the entery poin of the program
+ * Return: The program returns 0 after successfull run
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		printf("%d", number);
+	}
+	printf("\n");
+
+	return (0);
+}
