ca522e2437c659206179a9eb2abfef103037e783
brainCosby <braincosby777@gmail.com>
2023-07-16 18:18:17 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - The main function is the entry point of the program
+ * Return: The program returns 0 after successfull run
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+
+	return (0);
+}
