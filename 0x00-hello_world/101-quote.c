788898129d416de134f885b7403f194284f401ef
brainCosby <braincosby777@gmail.com>
2023-07-16 19:25:41 -0700

    UNIX

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entery point
+ * Return: 1 on success
+ */
+int main(void)
+{
+	write(1,"and that place of art is useful\" - Dora Kopar, 2015-10-19\n", 59);
+
+	return (1);
+}
