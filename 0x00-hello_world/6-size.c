44a202a1557767bfebdfdd5cd7c975b46fd14cc6
brainCosby <braincosby777@gmail.com>
2023-07-16 18:37:05 -0700

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - The main function is the entery point of the program
+ * Return: Te program returns 0 after successfull run
+ */
+int main(viod)
+{
+	pintf("Size of a char: %d byte(s)\n", sizeof(char));
+	pintf("Size of a int: %d byte(s)\n", sizeof(int));
+	pintf("Size of a long: %d byte(s)\n", sizeof(long));
+	pintf("Size of a long long: %d byte(s)\n", sizeof(long long));
+	pintf("Size of a float: %d byte(s)\n", sizeof(float));
+
+	return (0);
+}
