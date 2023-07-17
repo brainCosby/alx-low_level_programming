630751a0084d8bbb704ed32a9be30b561a1f9b22
brainCosby <braincosby777@gmail.com>
2023-07-16 18:46:00 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
  */
 int main(viod)
 {
-	pintf("Size of a char: %d byte(s)\n", sizeof(char));
-	pintf("Size of a int: %d byte(s)\n", sizeof(int));
-	pintf("Size of a long: %d byte(s)\n", sizeof(long));
-	pintf("Size of a long long: %d byte(s)\n", sizeof(long long));
-	pintf("Size of a float: %d byte(s)\n", sizeof(float));
+	printf("Size of a char: %d byte(s)\n",  (int)sizeof(char));
+	printf("Size of a int: %d byte(s)\n",  (int)sizeof(int));
+	printf("Size of a long: %d byte(s)\n", (int)sizeof(long));
+	printf("Size of a long long: %d byte(s)\n", (int)sizeof(long long));
+	printf("Size of a float: %d byte(s)\n", (int)sizeof(float));
 
 	return (0);
 }
