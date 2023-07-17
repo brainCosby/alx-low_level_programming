fd67aa93f469971ccabf8b57ab69bf1c4089896f
brainCosby <braincosby777@gmail.com>
2023-07-16 18:11:08 -0700

    Puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle, followed by a new line");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
