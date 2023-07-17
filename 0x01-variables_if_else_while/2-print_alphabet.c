757b26d66483ce05d9de22c0c8560fad2fc20531
brainCosby <braincosby777@gmail.com>
2023-07-17 16:17:14 -0700

    Print Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdliv.h>
+#include <time.h>
+
+/**
+ * main - The main function is the entry point of the program
+ * Return: The program returns 0 after successfull run
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar("\n");
+
+	return (0);
+}
