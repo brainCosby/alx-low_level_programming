f7eaaaf7e3d115c27ada521b75d53ab8b66098f6
brainCosby <braincosby777@gmail.com>
2023-07-16 17:46:12 -0700

    Puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - The main function is the entry point of the programm
+ * Return: The program returns 0 after successfull run
+ */
+int main(void)
+{
+	put("Programming is like building a multilingual puzzle, 
+	followed by a new line")
+	
+	return (0);
+}
