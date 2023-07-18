244455316626dce876c23d1fcd82a59299b9e442
brainCosby <braincosby777@gmail.com>
2023-07-17 17:49:25 -0700

    The less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,9 +10,9 @@ int main(void)
 	int num1;
 	int num2;
 
-	for (num1 = 0; num1 < 10 num1++)
+	for (num1 = 0; num1 < 10; num1++)
 	{
-		for (num2 = num1 + 1; num2 <= 9; mum2++)
+		for (num2 = num1 + 1; num2 <= 9; num2++)
 		{
 			putchar('0' + num1);
 			putchar('0' + num2);
