81661e936d0efba073298eba1418eda09fce3f7f
brainCosby <braincosby777@gmail.com>
2023-07-18 09:11:18 -0700

    Print the alphabets x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabets 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i = 10;
+	char alpha;
+
+	while (i < 11)
+	{
+		char alpha = 'a';
+
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+		}
+	}
+	_putchar('\n');
+}
