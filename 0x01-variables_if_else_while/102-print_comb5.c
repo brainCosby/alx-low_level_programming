a1d975e671d88de37b6f5c9c5e1694d37cff65d5
brainCosby <braincosby777@gmail.com>
2023-07-17 18:26:06 -0700

    software is eating the world

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - The fuunction main is the entry point of the program
+ * Return: The program returns 0 after successfull run
+ */
+int main(void)
+{
+	int nun1;
+	int num2;
+
+	for (num1 = 0; num1 <= 99; num1++)
+	{
+		for (num2 = num1; num2 <= 99; num2++)
+		{
+			if (num1 != num2)
+			{
+				putchar('0' + num1 / 10);
+				putchar('0' + num1 % 10);
+				putchar(' ');
+				putchar('0' + num2 / 10);
+				putchar('0' + num2 % 10);
+			}
+			if (num1 != 98 || num2 != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
