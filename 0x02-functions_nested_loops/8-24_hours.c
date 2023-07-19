c77ae25d5a3838ea0b2cd75ed336a22667eda28b
brainCosby <braincosby777@gmail.com>
2023-07-18 17:14:34 -0700

    Every minute

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Print the every minute of the day
+ */
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k <= 5; k++)
+			{
+				for (l = 0; l <= 9; l++)
+				{
+					if (i >= 2 && b >= 4)
+					{
+						_putchar(a + 48);
+						_putchar(b + 48);
+						_putchar(58);
+						_putchar(c + 48);
+						_putchar(d + 48);
+						_putchar('\n');
+					}
+				}
+			}
+		}
+	}
+}
