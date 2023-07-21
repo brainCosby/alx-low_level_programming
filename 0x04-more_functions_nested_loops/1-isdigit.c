700fe88c325b322b3ee3d318f6e7e3a77f26c654
brainCosby <braincosby777@gmail.com>
2023-07-21 13:50:40 -0700

    Is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers
+ * @a: First parameter
+ * @b: Second parameter
+ * Return: mul (result)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
