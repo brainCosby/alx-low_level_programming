289a9c813bb992f290dd3401551db268fe17c722
brainCosby <braincosby777@gmail.com>
2023-07-21 14:26:06 -0700

    Print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,13 +3,11 @@
 /**
  * print_numbers - prints numbers from 0 to 9
  */
-
-
 void print_numbers(void)
 {
 	int number;
 
-	for (number = 48; number < 57; number++)
+	for (number = 48; number <= 57; number++)
 	{
 		_putchar(number);
 	}
