5036f8098213ccf75a749305620ad756fdc3caf3
brainCosby <braincosby777@gmail.com>
2023-08-07 03:50:38 -0700

    debug 1

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,9 +16,10 @@ int main(int argc, char *argv[])
 		mul = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", mul);
 	}
-	else if (argc == 1)
+	else
 	{
 		printf("Error\n");
+		return (1);
 	}
 
 	return (0);
