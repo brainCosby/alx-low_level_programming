1bb723943816f23fbcfe4c0b063455b5e72cd654
brainCosby <braincosby777@gmail.com>
2023-08-07 03:26:10 -0700

    A program the multiplies 2 command line number arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point (multiply two numbers passed as command line arguments)
+ * @argc: Number of command line arguments parssed
+ * @argv: An array of the command line arguments passed
+ * Return: Reatun 0 after successful run
+ */
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc > 1)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
+	else if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
