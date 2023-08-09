4ad88a8882856115ecb1a21bd801d4c81cfdeb16
brainCosby <braincosby777@gmail.com>
2023-08-09 09:33:44 -0700

    a program that allocated memory for 2D arrays

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -4,15 +4,14 @@
 /**
  * alloc_grid - allocate  space for  to a 2 dimensional array of integers
  * @width: the number of columns in the grid
- * @height: the number of rows int he grid
+ * @height: the number of rows int the grid
  * Return: returns a pointer to a 2 dimensional array of integers,
  * return NULL on failure,
  * If width or height is 0 or negative, return NULL
  */
 int **alloc_grid(int width, int height)
 {
-	int i, a;
-	int b;
+	int i, j;
 	int **two_dim_array;
 
 	/* if width or height is 0 or negative */
@@ -20,28 +19,36 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-
 	/* allocace memory for the 2D array */
 	two_dim_array = (int **)malloc(height * sizeof(int));
-
-	for (i = 0; i < height; i++)
+	if (two_dim_array != NULL)
 	{
-		two_dim_array[i] = (int *)malloc(width * sizeof(int));
+		/*Allocate memory for each row and initialize elements to 0 */
+		for (i = 0; i < height; i++)
+		{
+			two_dim_array[i] = (int *)malloc(width * sizeof(int));
+			if (two_dim_array[i] == NULL)
+			{
+				/* Handle memory allocation failure for individual rows */
+				/* Free previously allocated memory before returning NULL */
+				for (j = 0; j < i; j++)
+				{
+					free(two_dim_array[j]);
+				}
+				free(two_dim_array);
+				return (NULL);
+			}
+			/* Initialize elements to 0 */
+			for (j = 0; j < width; j++)
+			{
+				two_dim_array[i][j] = 0;
+			}
+		}
+		return (two_dim_array);
 	}
-	/* if it fails to create the space for the 2D array */
-	if (two_dim_array == NULL)
+	else
 	{
+		/* Handle memory allocation failure for the array of row pointers */
 		return (NULL);
 	}
-
-	/* initialize all elements of the 2D array to 0 */
-	for (a = 0; a < height; a++)
-	{
-		for (b = 0; b < width; b++)
-		{
-			two_dim_array[a][b] = 0;
-		}
-	}
-
-	return (two_dim_array);
 }
