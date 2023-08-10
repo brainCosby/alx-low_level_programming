83fb89ed38fed7d4e5bd7405c9dbd81668e94a42
brainCosby <braincosby777@gmail.com>
2023-08-09 17:07:17 -0700

    A program to free the memory after dynamic grit memory allocation

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2 dimensional grid previously created by your alloc_grid function.
+ * @grid: the grid to free
+ * @height: the height of the grid
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid != NULL)
+	{
+		for (i = 0; i < height; i++)
+		{
+			free(grid[i]);
+		}
+		free(grid);
+	}
+}
