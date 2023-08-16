81c58562a69cfcbddb65a2008845565c479e83f7
brainCosby <braincosby777@gmail.com>
2023-08-15 20:43:43 -0700

    a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure that information on a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: a structure called dog with members name,
+ * age, and owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
