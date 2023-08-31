d637cb0f13db1514e3cdf6e425893973758fee63
brainCosby <braincosby777@gmail.com>
2023-08-30 18:11:32 -0700

    listint_t *current = *head;

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,7 +3,7 @@
 #include "lists.h"
 
 /**
- * free_listint - Frees a linked list.
+ * free_listint2 - Frees a linked list.
  * @head: Frees a listint_t list and sets the head to NULL.
  */
 void free_listint2(listint_t **head)
